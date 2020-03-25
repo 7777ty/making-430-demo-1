57cf4f8e3a2a1b055f1af70da2fb6f948616a7b8
7777ty <568318359@qq.com>
2020-03-25 22:15:58 +0800

    添加按钮click事件

--- src/Button.vue
+++ src/Button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" >
-        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
-        <g-icon class="loading" name="dengdai"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <g-icon v-if="icon  &&! loading" :name="icon " class="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading"  name="dengdai"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -13,6 +13,10 @@
         name:'',
         props:{
             icon:{},
+            loading:{
+                type:Boolean,
+                default:false,
+            },
             iconPosition:{
                 type:String,
                 default:'left',
