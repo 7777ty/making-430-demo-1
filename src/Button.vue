d926babf05650a92fc6925a084fe0ce83891df7a
7777ty <568318359@qq.com>
2020-03-25 22:03:13 +0800

    添加按键的loading状态

--- src/Button.vue
+++ src/Button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" >
         <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+        <g-icon class="loading" name="dengdai"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -24,6 +25,11 @@
 </script>
 
 <style lang='scss' scoped>
+    @keyframes spin {
+        0%{transform: rotate(0deg)}
+        100%{transform: rotate(360deg)}
+
+    }
     .g-button{
         font-size: var(--font-size);
         height: var(--button-height);
@@ -62,6 +68,9 @@
             }
         }
 
+    }
+    .loading{
+        animation: spin 2s linear infinite;
     }
 
 </style>
