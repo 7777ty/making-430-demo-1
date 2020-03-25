e7836dddc313f072be8818d299ee9eb9f4b419dd
7777ty <568318359@qq.com>
2020-03-25 21:57:40 +0800

    组件化了Icon

--- src/Button.vue
+++ src/Button.vue
@@ -1,10 +1,8 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" >
-        <svg v-if="icon" class="icon" >
-            <use :xlink:href=`#icon-${icon}`></use>
-        </svg>
+        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
         <div class="content">
-            <slot/>
+            <slot></slot>
         </div>
     </button>
 </template>
