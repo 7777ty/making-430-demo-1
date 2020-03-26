8c6c96f9a3b9a6391bee3333bf745ee929e96cc6
7777ty <568318359@qq.com>
2020-03-26 12:36:20 +0800

    检测Buttongroup的子元素应该是button

--- src/Button-group.vue
+++ src/Button-group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script lang='ts'>
-
+    export default {
+        mounted(){
+            for(let node of this .$el.children){
+                let name =node.nodeName.toLowerCase();
+                if (name!=='button'){
+                    console.warn(`g-button-group的子元素应该全是g-button但你写的是${name}`)
+                }
+            }
+        }
+    }
 </script>
 
 <style lang='scss' scoped>
